d318bf4ebc42b5ff05dbd9075c45a0fd30122a37
notend <xyqinsong@163.com>
2023-05-05 13:50:55 +0800

    chore: remove vuepress config folder

--- src/.vuepress/theme/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './other'
-export * from './registerGlobalComponents'
-export * from './createOneColor'
--- src/.vuepress/theme/utils/other.ts
+++
@@ -1,6 +0,0 @@
-export function toISODate(d: number | string | Date) {
-  // @ts-ignore
-  const ISODate = new Date(d)
-  ISODate.setHours(ISODate.getHours(), ISODate.getMinutes() - ISODate.getTimezoneOffset())
-  return ISODate.toISOString().replace("T", " ").replace("Z", "").split(".")[0]
-}
