09510c6eafb74732f04b39a8c34828254a48d1ee
notend <xyqinsong@163.com>
2023-11-27 22:44:38 +0800

    chore: update theme

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,5 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+// https://vitepress.dev/guide/extending-default-theme#customizing-css
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
 
-export default {
-  ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-}
+export default DefaultTheme
